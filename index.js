17100058cc2e2de88221d9e7db0cd14f91378353
saaya-code <boualimohamed74@gmail.com>
2023-06-09 12:14:48 +0100

    fixes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = require('./db/connect');
 const fs = require('node:fs');
 const path = require('node:path');
+const { channel } = require('node:diagnostics_channel');
 require("dotenv").config();
 const TOKEN = process.env.TOKEN;
 const client = new Client({
@@ -46,9 +47,14 @@ client.once(Events.ClientReady, (client) => {
 client.on(Events.PresenceUpdate, async (oldPresence, newPresence) => {
 	if (newPresence.member.user.bot) return;
 	if (newPresence.member.user.id != '352941142876225536') return;
+	try{
 	const room = "1116682855301533706";
 	const channel = client.channels.cache.get(room);
 	channel.send(`${newPresence.member.user.username} was __**${oldPresence.status}**__ and became__**${newPresence.status}__** at ${new Date().toLocaleString()}`);
+	}catch(err){
+		channel.send("There was an error but presenance was updated at "+new Date().toLocaleString());
+		console.log(err);
+	}
 });
 
 client.login(TOKEN);
